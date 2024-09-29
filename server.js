b7b0c28d94772e256ab37aaa447aabc61f2fe0d0
asoro-git <ohvelynuna@gmail.com>
2024-09-29 10:19:34 +1000

    testing commit

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require("path");
 const port = 3000;
 const socketio = require("socket.io");
+const mongoose = require("mongoose");
 
 app.use("/static", express.static("static"));
+
+//connect to mongoose
 
 // start a http server
 const http = require("http");
@@ -22,8 +25,9 @@ app.get("/roadmap", (req, res) => {
   res.sendFile(path.join(__dirname, "roadmap.html"));
 });
 
-//io receive signal from frontend
-//io emit data to frontend
+// //io receive signal from frontend
+// io.on("connection", console.log("socket connected!"));
+// //io emit data to frontend
 
 const PORT = process.env.PORT || 3000; // Fallback to 3000 for local dev
 server.listen(PORT, () => {
